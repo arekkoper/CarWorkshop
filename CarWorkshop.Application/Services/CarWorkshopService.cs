da88c5b55a905a61eb2bda3e29e93bb54a2af3e0
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2023-07-05 20:51:08 +0200

    add automapper and readmodel

--- CarWorkshop.Application/Services/CarWorkshopService.cs
+++ CarWorkshop.Application/Services/CarWorkshopService.cs
@@ -1,4 +1,6 @@
-﻿using CarWorkshop.Domain.Interfaces;
+﻿using AutoMapper;
+using CarWorkshop.Application.CarWorkshop;
+using CarWorkshop.Domain.Interfaces;
 
 
 namespace CarWorkshop.Application.Services
@@ -6,14 +8,18 @@ namespace CarWorkshop.Application.Services
     public class CarWorkshopService : ICarWorkshopService
     {
         private readonly ICarWrokshopRepository _carWrokshopRepository;
+        private readonly IMapper _mapper;
 
-        public CarWorkshopService(ICarWrokshopRepository carWrokshopRepository)
+        public CarWorkshopService(ICarWrokshopRepository carWrokshopRepository, IMapper mapper)
         {
             _carWrokshopRepository = carWrokshopRepository;
+            _mapper = mapper;
         }
 
-        public async Task Create(Domain.Entities.CarWorkshop carWorkshop)
+        public async Task Create(CarWorkshopDTO carWorkshopDTO)
         {
+            var carWorkshop = _mapper.Map<Domain.Entities.CarWorkshop>(carWorkshopDTO);
+
             carWorkshop.EncodeName();
             await _carWrokshopRepository.Create(carWorkshop);
 
