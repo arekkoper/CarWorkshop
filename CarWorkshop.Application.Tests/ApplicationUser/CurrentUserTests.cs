286fc54ae55003b5c6f2adb0795e1460c9c0e3b6
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2023-07-15 14:06:47 +0200

    add some first tests

---
+++ CarWorkshop.Application.Tests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using CarWorkshop.Application.ApplicationUser;
+using FluentAssertions;
+
+namespace CarWorkshop.Application.ApplicationUser.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void IsInRole_WithMatchingRoleShouldReturnTrue()
+        {
+            //arrange
+            var currentUser = new CurrentUser("1", "test@test.com", new List<string>(){ "Admin", "User" });
+
+            //act
+            var isInRole = currentUser.IsInRole("Admin");
+
+            //assert
+            isInRole.Should().BeTrue();
+        }
+        
+        [Fact()]
+        public void IsInRole_WithNonMatchingRoleShouldReturnFalse()
+        {
+            //arrange
+            var currentUser = new CurrentUser("1", "test@test.com", new List<string>(){ "Admin", "User" });
+
+            //act
+            var isInRole = currentUser.IsInRole("Manager");
+
+            //assert
+            isInRole.Should().BeFalse();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNonMatchingCaseRoleShouldReturnFalse()
+        {
+            //arrange
+            var currentUser = new CurrentUser("1", "test@test.com", new List<string>() { "Admin", "User" });
+
+            //act
+            var isInRole = currentUser.IsInRole("admin");
+
+            //assert
+            isInRole.Should().BeFalse();
+        }
+    }
+}
---
+++ CarWorkshop.Domain.Tests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using CarWorkshop.Domain.Entities;
+using FluentAssertions;
+
+namespace CarWorkshop.Domain.Entities.Tests
+{
+    public class CarWorkshopTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            //arrange
+            var carWorkshop = new CarWorkshop();
+            carWorkshop.Name = "Test CarWorkshop";
+
+            //act
+            carWorkshop.EncodeName();
+
+            //assert
+            carWorkshop.EncodedName.Should().Be("test-carworkshop");
+        }
+
+        [Fact]
+        public void EncodeName_ShouldThrowExeptionWhenNameIsNull()
+        {
+            //arrange
+            var carWorkshop = new CarWorkshop();
+
+            //act
+            Action action = () => carWorkshop.EncodeName();
+
+            //assert
+            action.Invoking(a => a.Invoke()).Should().Throw<NullReferenceException>();
+        }
+    }
+}
