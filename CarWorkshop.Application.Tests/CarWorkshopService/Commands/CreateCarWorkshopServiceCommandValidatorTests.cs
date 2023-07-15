74c1501051001a0d437046482771fac8784a0976
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2023-07-15 18:40:26 +0200

    add tests for validator

---
+++ CarWorkshop.Application.Tests/CarWorkshopService/Commands/CreateCarWorkshopServiceCommandValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using CarWorkshop.Application.CarWorkshopService.Commands;
+using FluentValidation.TestHelper;
+
+namespace CarWorkshop.Application.CarWorkshopService.Commands.Tests
+{
+    public class CreateCarWorkshopServiceCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            //arrange
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "100",
+                Description = "Wymiana opon",
+                CarWorkshopEncodedName = "wymiana-opon"
+            };
+
+            //act
+            var result = validator.TestValidate(command);
+
+            //assert
+            result.ShouldNotHaveAnyValidationErrors();
+        } 
+        
+        [Fact()]
+        public void Validate_WithInvalidCommand_ShouldHaveValidationError()
+        {
+            //arrange
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand()
+            {
+                Cost = "",
+                Description = "",
+                CarWorkshopEncodedName = null
+            };
+
+            //act
+            var result = validator.TestValidate(command);
+
+            //assert
+            result.ShouldHaveValidationErrorFor(c => c.Cost);
+            result.ShouldHaveValidationErrorFor(c => c.Description);
+            result.ShouldHaveValidationErrorFor(c => c.CarWorkshopEncodedName);
+        }
+    }
+}
