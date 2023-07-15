7c7c51befa0de485538ccaa17d56113c0a397e2e
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2023-07-15 19:17:15 +0200

    add test for mapper

---
+++ CarWorkshop.Application.Tests/Mappings/CarWorkshopMappingProfileTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using CarWorkshop.Application.Mappings;
+using Moq;
+using CarWorkshop.Application.ApplicationUser;
+using AutoMapper;
+using CarWorkshop.Application.CarWorkshop;
+using FluentAssertions;
+
+namespace CarWorkshop.Application.Mappings.Tests
+{
+    public class CarWorkshopMappingProfileTests
+    {
+        [Fact()]
+        public void MappingProfile_ShouldMapCarWorkshopDtoToCarWorkshop()
+        {
+            //arrange
+            var userContextMock = new Mock<IUserContext>();
+            userContextMock.Setup(c => c.GetCurrentUser()).Returns(new CurrentUser("1", "test@test.com", new[] { "User" }));
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new CarWorkshopMappingProfile(userContextMock.Object)));
+
+            var mapper = configuration.CreateMapper();
+
+            var dto = new CarWorkshopDTO()
+            {
+                City = "Wałcz",
+                PhoneNumber = "321654987",
+                PostalCode = "70-600",
+                Adress = "Wąska 3/1"
+            };
+
+            //act
+            var result = mapper.Map<Domain.Entities.CarWorkshop>(dto);
+
+            //assert
+            result.Should().NotBeNull();
+            result.ContactDetails.City.Should().Be(dto.City);
+            result.ContactDetails.PhoneNumber.Should().Be(dto.PhoneNumber);
+            result.ContactDetails.PostalCode.Should().Be(dto.PostalCode);
+            result.ContactDetails.Adress.Should().Be(dto.Adress);
+
+        }
+    }
+}
