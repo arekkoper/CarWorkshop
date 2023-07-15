9a5773ce11b28e7c9ea3de63f727f5875a852176
Arkadiusz Koper <arkadiusz.koper1990@gmail.com>
2023-07-15 09:00:17 +0200

    you can now send notifications using toastr

---
+++ CarWorkshop.MVC/Extensions/ControllerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CarWorkshop.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarWorkshop.MVC.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static void SetNotification(this Controller controller, string type, string massage)
+        {
+            var notification = new Notification(type, massage);
+            controller.TempData["Notification"] = JsonConvert.SerializeObject(notification);
+        }
+    }
+}
